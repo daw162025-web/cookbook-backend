fb6c7c8e6ec9537bd81bb384f7a55e37bd309ed6
Telma <daw16.2025@gmail.com>
2026-04-12 17:26:18 +0200

    fix: app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\CorsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,12 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
-
-        $middleware->validateCsrfTokens(except: [
-            'api/*',
-        ]);
-
-        $middleware->prepend(CorsMiddleware::class); // ← primero de todo
+        $middleware->validateCsrfTokens(except: ['api/*']);
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
